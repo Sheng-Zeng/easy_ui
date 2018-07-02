7cd9abb514f773042b97782eba31fd4734549158
zengsheng <545215325@qq.com>
2018-07-02 19:08:07 +0800

    easy_ui练习

--- src/com/zs/entity/Admin.java
+++ src/com/zs/entity/Admin.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 public class Admin implements Serializable {
     private String username;
     private String password;
+    private String url;
 
     public Admin() {
     }
